62d59639734b579190c71d08a941549886235ce8
DolbyYu <yuaisheng@live.com>
2020-04-20 15:58:55 +0800

    Add comment

--- MyCircularQueue.h
+++ MyCircularQueue.h
@@ -8,12 +8,27 @@
 
 class MyCircularQueue {
 public:
+    /** Initialize your data structure here. Set the size of the queue to be k. */
     explicit MyCircularQueue(int k);
+     /** Insert an element into the circular queue. Return true if the operation is successful. */
     bool enQueue(int value);
+    /** Delete an element from the circular queue. Return true if the operation is successful. */
     bool deQueue();
+    /** Get the front item from the queue. */
+
+
     int Front();
+    /** Get the last item from the queue. */
+
+
     int Rear();
+    /** Checks whether the circular queue is empty or not. */
+
+
     bool isEmpty();
+    /** Checks whether the circular queue is full or not. */
+
+
     bool isFull();
 
 private:
